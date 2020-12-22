5bda6d8548d6ab698774fbee7cf81fd760d0e5b7
Andrej <a.user.de@gmail.com>
2020-12-22 16:25:15 +0100

    deploy

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = global.config = {
-    mainsite: "https://xprilion.com"
-    // other global config variables you wish
-};
+  mainsite: 'login',
+  // other global config variables you wish
+}
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,8 +3,19 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  //Your config values
-};
+  // apiKey: process.env.RAMLY_API_KEY,
+  // authDomain: process.env.RAMLY_AUTH_DOMAIN,
+  // projectId: process.env.RAMLY_PROJECT_ID,
+  // storageBucket: process.env.RAMLY_STORGAE_BUCKET,
+  // messagingSenderId: process.env.RAMLY_MESAGING_SENDER_ID,
+  // appId: process.env.RAMLY_APP_ID,
+  apiKey: 'AIzaSyAR_v0YNidQSzUyUnqNpfwAz_20fFPKzaA',
+  authDomain: 'ramly-dev.firebaseapp.com',
+  projectId: 'ramly-dev',
+  storageBucket: 'ramly-dev.appspot.com',
+  messagingSenderId: '833771840186',
+  appId: '1:833771840186:web:c88b108fdd4c3a2457d883',
+}
 
 export const myFirebase = firebase.initializeApp(firebaseConfig);
 const baseDb = myFirebase.firestore();
